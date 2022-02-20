46b5417c5c29c272e9e39d62d35637e2dad83e80
Jaseela99 <jaseelachittur@gmail.com>
2022-02-20 15:44:06 +0530

    added delte

--- routers/UserRouter.js
+++ routers/UserRouter.js
@@ -3,6 +3,8 @@ const userCtrl = require("../controllers/userCtrl")
 const router =(app)=>{
     app.post("/user",userCtrl.register)
     app.get("/user",userCtrl.getAllUsers)
+    app.get("/user/:id",userCtrl.getUserById)
+    app.delete("/user/:id",userCtrl.deleteUserById)
 }
 
 
