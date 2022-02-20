f4c397dd199c2761787d80d5f21cee3d14282cfb
Jaseela99 <jaseelachittur@gmail.com>
2022-02-20 09:16:01 +0530

    connected database

--- index.js
+++ index.js
@@ -2,10 +2,21 @@ const express =require("express")
 
 const app = express()
 
-const PORT =process.env.PORT||9000;
+const mongoose = require("mongoose")
 
-app.listen(PORT,()=>console.log('server is live on port 9000'))
+//connecting to mongodb
+try{
+    
+    mongoose.connect("mongodb+srv://JASEELA:JASEELA@cluster0.d42fr.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
+    //if conne tion is successful
+    console.log("database connected")
+
+}catch(error){
+    
+    console.log(error)
+}
 
 
-
-
+const Port =process.env.PORT||9000;
+//server listening to port 9000
+app.listen(Port,()=>console.log('server is live on port 9000'))
