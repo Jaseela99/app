5bf04bb505ad6a9d413c2a60781350e8f75d4a49
Jaseela99 <jaseelachittur@gmail.com>
2022-02-21 15:14:52 +0530

    added models

--- models/Imagemodel.js
+++ models/Imagemodel.js
@@ -1,20 +1,40 @@
-const mongoose =require("mongoose")
+const mongoose = require("mongoose");
+const { ObjectId } = mongoose.Schema.Types;
 
-const ImageSchema = new mongoose.Schema(
-    {
-     category:{
-         type:String
-     },
-     path:{
-         type:String
-     },
-     likes:{
-         type:Number,
-         default:0
-     },
-     comments:{
-         type:String
-     }
-    })
-    const ImageModel = mongoose.model("Images",ImageSchema)
-    module.exports =ImageModel
+const imageSchema = new mongoose.Schema(
+  {
+    category: {
+      type: String,
+    },
+    path: {
+      type: String,
+    },
+    likes: [
+      {
+        type: ObjectId,
+        ref: "User",
+      },
+    ],
+    likeCount: {
+      type: Number,
+      default: 0,
+    },
+    comments: [
+      {
+        type: ObjectId,
+        ref: "Comment",
+      },
+    ],
+    commentCount: {
+      type: Number,
+      default: 0,
+    },
+    user: {
+      type: ObjectId,
+      ref: "User",
+    },
+  },
+  { timestamps: true }
+);
+const imageModel = mongoose.model("Images", imageSchema);
+module.exports = imageModel;
--- models/Usermodel.js
+++ models/Usermodel.js
@@ -1,25 +1,40 @@
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
+const { ObjectId } = mongoose.Schema.Types;
 
-const userSchema = new mongoose.Schema({
- fullName :{
-     type:String,
-     required:true
- },
- email:{
-     type:String,
-     lowercase:true,
-     required:true,
-     unique:true
- },
- password :{
-     type:String,
-     required:true
- }
+const userSchema = new mongoose.Schema(
+  {
+    fullName: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      lowercase: true,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    posts: [
+      {
+        type: ObjectId,
+        ref: "Images",
+      },
+    ],
+    postCount: {
+      type: Number,
+      default: 0,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
 
-})
+//creating user model (collection and schema combines to form model)
 
-//creating user model
+const userModel = mongoose.model("User", userSchema);
 
-const userModel =mongoose.model("User",userSchema)
-
-module.exports =userModel
+module.exports = userModel;
