8317498eaac9bd0a334c7b01ee0c947b6784e4fb
Jaseela99 <jaseelachittur@gmail.com>
2022-02-21 15:02:11 +0530

    changes made

--- index.js
+++ index.js
@@ -1,33 +1,30 @@
-const express =require("express")
+const express = require("express");
 
-const app = express()
+const app = express();
 
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
 
-//middleware
-app.use(express.json())
+require("dotenv").config();
 
-require("./routers/UserRouter")(app) //since it is a function
+app.use(express.json());
+
+require("./routers/UserRouter")(app); //since it is a function
+require("./routers/ImageRouter")(app);
 
 //connecting to mongodb
-mongoose.connect("mongodb+srv://JASEELA:JASEELA@cluster0.d42fr.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
+mongoose
+  .connect(
+    "mongodb+srv://JASEELA:JASEELA@cluster0.d42fr.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+  )
 
-//if connection is successful
-//it is a promise
- .then(()=>
-    console.log("database connected")
-
- )
- .catch((error)=>
-    
-    console.log(error))
-
+  //if connection is successful
+  //it is a promise
+  .then(() => console.log("database connected"))
+  .catch((error) => console.log(error));
 
 //get method
-app.get("/",(req,res)=>
-res.send("hello people")
-)
+app.get("/", (req, res) => res.send("hello people"));
 
-const Port =process.env.PORT||9000;
+const Port = process.env.PORT || 8000;
 //server listening to port 9000
-app.listen(Port,()=>console.log('server is live on port 9000'))
+app.listen(Port, () => console.log("server is live on port 9000"));
